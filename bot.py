7d51d9880097239b2c67ae41b37b964f36be5193
Fyks <kotanic7@gmail.com>
2017-03-01 21:41:09 +0200

    telegram requests function

--- bot.py
+++ bot.py
@@ -13,13 +13,13 @@ def get(method, params):
     return requests.get(URL + method, params=params)
 
 
-def tel_request(params):
-    response = get('getUpdates', params=params)
+def tel_request(method, params):
+    response = get(method, params=params)
     return response.json()['result']
 
 
 def send_message(chat_id, reply_to_message_id):
-    get('sendMessage', params={
+    tel_request('sendMessage', params={
         'chat_id': chat_id,
         'reply_to_message_id': message_id,
         'text': text
@@ -27,27 +27,25 @@ def send_message(chat_id, reply_to_message_id):
 
 
 def send_sticker(chat_id, reply_to_message_id, sticker):
-    get('sendSticker', params={
+    tel_request('sendSticker', params={
         'chat_id': chat_id,
         'reply_to_message_id': message_id,
         'sticker': sticker
     })
 
 
-def rand_anime_sticker(chat_id, reply_to_message_id):
+def rand_anime_sticker():
     rand = random.randint(0, len(rlist.ANIME) - 1)
-    sticker = rlist.ANIME[rand]
-    send_sticker(chat_id, reply_to_message_id, sticker)
+    return rlist.ANIME[rand]
 
 
-def rand_cat_sticker(chat_id, reply_to_message_id):
+def rand_cat_sticker():
     rand = random.randint(0, len(rlist.CATS) - 1)
-    sticker = rlist.CATS[rand]
-    send_sticker(chat_id, reply_to_message_id, sticker)
+    return rlist.CATS[rand]
 
 
 def ping():
-    get('sendMessage', params={
+    tel_request('sendMessage', params={
         'chat_id': 237174923,
         'text': 'OK'
     })
@@ -61,7 +59,7 @@ if __name__ == '__main__':
 
     while True:
 
-        upd = tel_request(params={
+        upd = tel_request('getUpdates', params={
             'limit': LIMIT,
             'timeout': TIMEOUT,
             'offset': update_id + 1})
@@ -73,10 +71,11 @@ if __name__ == '__main__':
                 text = i['message']['text']
 
                 if re.search(r'Котяш', text):
-                    if re.search(r'няш', text):
-                        rand_anime_sticker(chat_id, message_id)
+                    if re.search(r'Котяш.+няш', text):
+                        send_sticker(chat_id, message_id, rand_anime_sticker())
                         break
-                    rand_cat_sticker(chat_id, message_id)
+                    send_sticker(chat_id, message_id, rand_cat_sticker())
+
             except KeyError:
                 pass
 
