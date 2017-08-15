7e61ec295ed1bc6d4983c607f1edef0bf629bebe
Fyks <kotanic7@gmail.com>
2017-08-15 09:05:31 +0300

    15.08.17 restrict and time 0.3

--- mute.py
+++ mute.py
@@ -1,9 +1,11 @@
 import requests
+import teltoken
 import time
 
 mute_list = {}
 mute_count = {}
 mute_timer = {}
+URL = "https://api.telegram.org/bot" + teltoken.TOKEN + '/'
 
 
 # restrict method
@@ -15,7 +17,8 @@ def restrict_user(chat_id, user_id, can_send_messages=None, can_send_media_messa
               'can_send_messages': can_send_messages,
               'can_send_media_messages': can_send_media_messages,
               'can_send_other_messages': can_send_other_messages}
-    return requests.get('restrictChatMember', params)
+    print(params)
+    return requests.get(URL + 'restrictChatMember', params)
 
 
 # return current unix time + 3 min
@@ -43,6 +46,7 @@ def del_message(chat_id, message_id):
 
 
 # mute list redactor
+# дописать таймер и оповещения к голосованию
 def gen(mute_list, mute_id, muter, chat_id):
     if mute_id in mute_list:
         if muter in mute_list[mute_id]:
@@ -51,8 +55,9 @@ def gen(mute_list, mute_id, muter, chat_id):
             mute_list[mute_id].append(muter)
             if len(mute_list[mute_id]) == 3:
                 restrict_user(chat_id, mute_id, timer(), False, False, False)
+                print('Done')
             else:
                 print(mute_list, str(3 - len(mute_list[mute_id])) + ' left')
     else:
         mute_list[mute_id] = [muter]
-        print(mute_list)
+        restrict_user(chat_id, mute_id, False, False, False, timer())
